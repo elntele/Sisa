448b1aaf77d83fc9d91eedee60a58cbb80bdcaf7
Jefferson Cabral <jcabral1500@gmail.com>
2017-06-15 17:29:00 -0300

    Finalização da tela de Login
    Conclusão de fragment_login.xml

--- app/src/main/java/br/ufrpe/android/sisa/LoginActivity.java
+++ app/src/main/java/br/ufrpe/android/sisa/LoginActivity.java
@@ -1,6 +1,8 @@
 package br.ufrpe.android.sisa;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -9,9 +11,18 @@ import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new LoginFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
--- app/src/main/java/br/ufrpe/android/sisa/LoginFragment.java
+++ app/src/main/java/br/ufrpe/android/sisa/LoginFragment.java
@@ -2,16 +2,39 @@ package br.ufrpe.android.sisa;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by jorge on 15/06/2017.
  */
 
 public class LoginFragment extends Fragment{
+
+
+    public EditText mEmailField;
+    public EditText mPasswordField;
+    public Button mButtonSignIn;
+    public Button mButtonGetOut;
+
     @Override
     public void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-    }
 
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle saveInstanceState){
+        View v = inflater.inflate(R.layout.fragment_login, parent, false);
+
+        mEmailField = (EditText) v.findViewById(R.id.email_edit_text);
+        mPasswordField = (EditText) v.findViewById(R.id.password_edit_text);
+        mButtonSignIn = (Button) v.findViewById(R.id.sign_in_button);
+        mButtonGetOut = (Button) v.findViewById(R.id.get_out_button);
+        return v;
+    }
 
 }
