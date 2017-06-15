d17df0013cd7b4e16db4298c702cb8299f1e2af7
Jefferson Cabral <jcabral1500@gmail.com>
2017-06-15 11:31:50 -0300

    Adição do novo activity_login.xml e da nova classe LoginActivity.java

---
+++ app/src/main/java/br/ufrpe/android/sisa/LoginActivity.java
@@ -0,0 +1,17 @@
+package br.ufrpe.android.sisa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LENOVO on 15/06/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
