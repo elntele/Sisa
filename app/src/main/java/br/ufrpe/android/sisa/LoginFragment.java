d9fdfdeeda0b21518536c0b069f045201b7c0911
Jorge Candeias do Nascimento <elntele@gmail.com>
2017-06-15 13:22:28 -0300

    botão login e cancela add, fazendo o activity do fragment

---
+++ app/src/main/java/br/ufrpe/android/sisa/LoginFragment.java
@@ -0,0 +1,17 @@
+package br.ufrpe.android.sisa;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jorge on 15/06/2017.
+ */
+
+public class LoginFragment extends Fragment{
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+    }
+
+
+}
