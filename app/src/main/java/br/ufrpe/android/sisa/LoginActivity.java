1a3c5bea14b5900dccd853bae495f8a3d7a950ae
Jorge Candeias do Nascimento <elntele@gmail.com>
2017-06-16 11:05:06 -0300

    explicação sobre a logica do fragment

--- app/src/main/java/br/ufrpe/android/sisa/LoginActivity.java
+++ app/src/main/java/br/ufrpe/android/sisa/LoginActivity.java
@@ -19,7 +19,9 @@ public class LoginActivity extends AppCompatActivity {
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
+        //parte do código onde o new LoginFragment que é uma nova instacia de Login fragmente
+        //é colocada dentri do activity_login.... em LoginFragmente é inflado o fragment_login.xml
+        //e aqui ele é cologado dentro do conteiner
         if (fragment == null){
             fragment = new LoginFragment();
             fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
