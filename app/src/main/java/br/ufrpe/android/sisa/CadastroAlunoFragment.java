724682d014692f3bbf06896b879e70dc91806b36
Jefferson Cabral <jcabral1500@gmail.com>
2017-06-19 12:06:12 -0300

    Adição dos arquivos CadastroAlunoFragment.java  e fragment_cadastro_aluno.xml
    Criação dos fragmentos para CadastroAluno

---
+++ app/src/main/java/br/ufrpe/android/sisa/CadastroAlunoFragment.java
@@ -0,0 +1,29 @@
+package br.ufrpe.android.sisa;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CadastroAlunoFragment extends Fragment {
+
+
+    public CadastroAlunoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cadastro_aluno, container, false);
+    }
+
+}
