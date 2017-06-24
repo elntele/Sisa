b695be3f1d74ff9d7db9f9ae70f660ab22f5dfcb
Jorge Candeias do Nascimento <elntele@gmail.com>
2017-06-24 19:43:12 -0300

    criando layout pra list intem disciplina

--- app/src/main/java/br/ufrpe/android/sisa/DisciplinaListFragment.java
+++ app/src/main/java/br/ufrpe/android/sisa/DisciplinaListFragment.java
@@ -2,7 +2,10 @@ package br.ufrpe.android.sisa;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -12,6 +15,7 @@ import java.util.ArrayList;
 
 public class DisciplinaListFragment extends ListFragment {
     private ArrayList<Disciplina> mDisciplinas;
+    private static final String TAG = "DisciplinaListFragment";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,12 @@ public class DisciplinaListFragment extends ListFragment {
         ArrayAdapter<Disciplina> adapter =new ArrayAdapter<Disciplina>(getActivity(),
                         android.R.layout.simple_list_item_1,mDisciplinas);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Disciplina d = (Disciplina) (getListAdapter()).getItem(position);
+        Log.d(TAG, d.getNome() + " foi clicado");
     }
 
 
