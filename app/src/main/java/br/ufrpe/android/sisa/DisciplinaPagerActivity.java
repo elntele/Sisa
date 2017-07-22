9d7fbb5a19df4abc7f04595d34847800b2183af8
Jefferson Cabral <jcabral1500@gmail.com>
2017-07-22 10:07:39 -0300

    Criação da classe DisciplinaPagerAdapter e seu respectivo layout
    Criação da classe DisciplinaPagerAdapter e seu respectivo layout.

---
+++ app/src/main/java/br/ufrpe/android/sisa/DisciplinaPagerActivity.java
@@ -0,0 +1,17 @@
+package br.ufrpe.android.sisa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LENOVO on 22/07/2017.
+ */
+
+public class DisciplinaPagerActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstaceState){
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.activity_disciplina_pager);
+    }
+}
