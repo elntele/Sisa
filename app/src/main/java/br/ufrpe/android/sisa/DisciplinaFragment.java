204990f3ba30419a85e61008876471f4e6b9a164
Jorge Candeias do Nascimento <elntele@gmail.com>
2017-08-28 00:33:37 -0300

    banco finalizado (criando tabelas disciplina e aluno)

--- app/src/main/java/br/ufrpe/android/sisa/DisciplinaFragment.java
+++ app/src/main/java/br/ufrpe/android/sisa/DisciplinaFragment.java
@@ -1,7 +1,9 @@
 package br.ufrpe.android.sisa;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -15,9 +17,14 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import br.ufrpe.android.sisa.database.AlunoBaseHelper;
+import br.ufrpe.android.sisa.database.SisaDbSchema;
 
 /**
  * Created by LENOVO on 05/07/2017.
@@ -107,16 +114,57 @@ public class DisciplinaFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext(), "Exemplo Toast", Toast.LENGTH_SHORT).show();
-
-            Intent intent = new Intent(getActivity(),TelaInicialActivity.class);
+              //  mDatabase = new AlunoBaseHelper(mContext)// adicionado para a tabela aluno
+                //        .getWritableDatabase();
+                inicia();
+                addAluno();
+                Intent intent = new Intent(getActivity(),TelaInicialActivity.class);
                 startActivity(intent);
             }
         });
+
+
 
 
      //   mFloatingActionButton = (FloatingActionButton) v.findViewById(R.id.cadastro_fab);
          // mSalvarButton = (Button) v.findViewById(R.id.salvar_button);
 
         return v;
+    }
+
+
+    private static ContentValues getContentValues(Aluno aluno) {
+        ContentValues values = new ContentValues();
+        values.put(SisaDbSchema.AlunoTable.Cols.ID, aluno.getId().toString());
+        values.put(SisaDbSchema.AlunoTable.Cols.NOME, aluno.getNome());
+        values.put(SisaDbSchema.AlunoTable.Cols.EMAIL, aluno.getEmail());
+        values.put(SisaDbSchema.AlunoTable.Cols.SENHA, aluno.getSenha());
+        values.put(SisaDbSchema.AlunoTable.Cols.CPF, aluno.getCpf());
+
+       // SharedPreferences mPrefs = null; // esse null foi adaptado
+       // SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(aluno.getCursadas() );
+       // prefsEditor.putString("", json);
+       // prefsEditor.commit();
+
+        values.put(SisaDbSchema.AlunoTable.Cols.CURSADAS, json);
+        values.put(SisaDbSchema.AlunoTable.Cols.AREA, aluno.getArea());
+        values.put(SisaDbSchema.AlunoTable.Cols.ANODEINGRESSO, aluno.getAndoDeIngresso());
+        values.put(SisaDbSchema.AlunoTable.Cols.PERIODODEINGRESSO, aluno.getPeriodoDeIngresso());
+        values.put(SisaDbSchema.AlunoTable.Cols.QTDDEPERIODOSTRANCADOS, aluno.getQtdDePeriodosTrancado());
+        return values;
+    }
+
+    public void inicia(){
+        mContext = getContext().getApplicationContext();
+        mDatabase = new AlunoBaseHelper(mContext)// adicionado para a tabela aluno
+                .getWritableDatabase();
+    }
+    public void addAluno(){
+
+        ContentValues values = getContentValues(mAluno);
+        mDatabase.insert(SisaDbSchema.AlunoTable.NAME, null, values);
+
     }
 }
